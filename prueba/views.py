2d2392f2ff20ef2acf7735cac6d18353ae1a8ca3
Marinavazmed <marina.vazquez.medinilla.al@iespoligonosur.org>
2023-01-28 19:06:14 +0100

    prueba fin

--- prueba/views.py
+++ prueba/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+import json
+from django.http import HttpResponseBadRequest, JsonResponse
+from prueba.models import *
 
 # Create your views here.
 def registro(request):
-    return render(request, 'prueba/index.html', {})
+    if(request.headers.get('X-Requested-With') == 'XMLHttpRequest'):
+        if request.method == 'POST':
+            data = json.load(request)
+            mapeado = Persona(nombre=data.get('nombre'), apellidos=data.get('apellidos'))
+            mapeado.save()
+            return JsonResponse({'status': 'Nueva persona añadida.'})
+        if request.method == 'GET':
+            personas = list(Persona.objects.all().values())
+            return JsonResponse({'context': personas})
+        return JsonResponse({'status': 'Registro fallido'}, status=400)
+    else:
+        return render(request, 'prueba/index.html', {})
