6ffeed9eaace3eed6d694b5f840d1ddcba38085b
Marinavazmed <marina.vazquez.medinilla.al@iespoligonosur.org>
2023-02-02 17:04:28 +0100

    rama nueva para pruebas

--- prueba/urls.py
+++ prueba/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.registro, name='personas'),
-    path('noticias/', views.noticias, name='noticias'),
+    path('', views.index, name='index'),
+    path('prueba_clase/', views.prueba_clase, name='prueba_clase'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- prueba/views.py
+++ prueba/views.py
@@ -6,37 +6,14 @@ from django.template.loader import render_to_string
 from prueba.models import *
 
 # Create your views here.
-def registro(request):
-    if(request.headers.get('X-Requested-With') == 'XMLHttpRequest'):
-        if request.method == 'POST':
-            data = json.load(request)
-            mapeado = Persona(nombre=data.get('nombre'), apellidos=data.get('apellidos'))
-            mapeado.save()
-            return JsonResponse({'status': 'Nueva persona añadida.'})
-
-        if request.method == 'GET':
-            personas = list(Persona.objects.all().values())
-            return JsonResponse({'context': personas})
-        return JsonResponse({'status': 'Registro fallido'}, status=400)
-    else:
-        return render(request, 'prueba/index.html', {})
-
-def noticias(request):
+def index(request):
+    return render(request, 'prueba/index.html', {})
+def prueba_clase(request):
     if(request.headers.get('X-Requested-With') == 'XMLHttpRequest'):
         if request.method == 'GET':
-            filtro = request.GET.get('filtro')
-            print(filtro)
-            if(filtro):
-                noticias=[
-                    {'titular': "Noticia 1", 'resumen': "Resumen de la noticia 1",
-                     'descripcion': "Esta es la noticia 1"}
-                ]
-            else:
-
-                noticias = [
-                    {'titular': "Noticia 1",'resumen': "Resumen de la noticia 1",'descripcion': "Esta es la noticia 1"},
-                    {'titular': "Noticia 2",'resumen': "Resumen de la noticia 2",'descripcion': "Esta es la noticia 2"},
-                    {'titular': "Noticia 3",'resumen': "Resumen de la noticia 3",'descripcion': "Esta es la noticia 3"}];
-
+            noticias = [
+                {'titular': "Noticia 1",'resumen': "Resumen de la noticia 1",'descripcion': "Esta es la noticia 1"},
+                {'titular': "Noticia 2",'resumen': "Resumen de la noticia 2",'descripcion': "Esta es la noticia 2"},
+                {'titular': "Noticia 3",'resumen': "Resumen de la noticia 3",'descripcion': "Esta es la noticia 3"}];
             return JsonResponse({'context':noticias})
     return render(request, 'prueba/noticias.html', {})
