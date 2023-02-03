7f1243f7533a66ba10e6c7609e101a800bcd506e
Marinavazmed <marina.vazquez.medinilla.al@iespoligonosur.org>
2023-02-03 13:44:04 +0100

    fin

--- prueba/static/js/examen.js
+++ prueba/static/js/examen.js
@@ -1,23 +1,36 @@
 $(document).ready(function () {
     /*Petición ajax*/
     $.ajax({
-        url: '/prueba_examen/',
+        url: '/prueba/',
         type: "GET",
         dataType: "json",
         success: (data) => {
-            $("body").append("<div id='todos-los-viajes'></div><h1>Subtitulo</h1><ul></ul>")
-            for(let i of data.j_son){
-                $("ul").first().append("<li></li>")
-                $("li").last().append("<img src='"+i.src+"'/><p>"+i.desc+"</p><ul class='specs'></ul>")
-                for(let j = 0; j<i.specs.length;j++){
-                    $(".specs").last().append("<li>"+i.specs[j]+"</li>")
-                }
-                $(".specs").last().append()
+            $("#contenido").append("<div id='todos-los-viajes'></div>")
+            $("#todos-los-viajes").append("<h1>Viajes Guía Incluido</h1><ul></ul>")
+            for (let i of data.j_son) {
+                $("ul").append("<li class='viaje-"+ i.clase_pais + "'></li>")
+                $(".viaje-"+i.clase_pais).append("<h2>"+i.ciudad_h2+"</h2><span class='detalle'>"+ i.detalle_precio_total +
+                " por " + i.detalle_num_noches + " noches</span>")
+                $(".viaje-"+i.clase_pais).append("<span class='por-noche'><span class='precio'> " + i.precio_noche + "</span>/noche </span>" )
+                $(".viaje-"+i.clase_pais).append("<button class=reserva>Reservalo ya!</button>")
+                $(".viaje-"+i.clase_pais).append("<li class='fotos'></li>")
+                $("li").last().append("<img src='"+ i.ruta_imagen + "'><span>"+i.pie_imagen + "</span>")
 
-                /*Ejercicio 9*/
+                $("img").last().hover(function(){
+                    $(".por-noche").addClass("descatado")
+
+                })
+
+                $("img").last().mouseleave(function(){
+                    $(".por-noche").removeClass("descatado")
+                })
+
                 $("img").last().click(function(){
-                    $(this).siblings().toggle()
-                });
+                    $(this).next().toggle()
+                })
+
+
+
             }
         },
         error: (error) => {
--- prueba/views.py
+++ prueba/views.py
@@ -8,24 +8,45 @@ from prueba.models import *
 # Create your views here.
 def index(request):
     return render(request, 'prueba/index.html', {})
-def prueba_clase(request):
-    if(request.headers.get('X-Requested-With') == 'XMLHttpRequest'):
-        if request.method == 'GET':
-            noticias = [
-                {'titular': "Noticia 1",'resumen': "Resumen de la noticia 1",'descripcion': "Esta es la noticia 1"},
-                {'titular': "Noticia 2",'resumen': "Resumen de la noticia 2",'descripcion': "Esta es la noticia 2"},
-                {'titular': "Noticia 3",'resumen': "Resumen de la noticia 3",'descripcion': "Esta es la noticia 3"}];
-            return JsonResponse({'context':noticias})
-    return render(request, 'prueba/noticias.html', {})
+# def prueba_clase(request):
+#     if(request.headers.get('X-Requested-With') == 'XMLHttpRequest'):
+#         if request.method == 'GET':
+#             noticias = [
+#                 {'titular': "Noticia 1",'resumen': "Resumen de la noticia 1",'descripcion': "Esta es la noticia 1"},
+#                 {'titular': "Noticia 2",'resumen': "Resumen de la noticia 2",'descripcion': "Esta es la noticia 2"},
+#                 {'titular': "Noticia 3",'resumen': "Resumen de la noticia 3",'descripcion': "Esta es la noticia 3"}];
+#             return JsonResponse({'context':noticias})
+#     return render(request, 'prueba/noticias.html', {})
 
 
-def examen(request):
+def prueba(request):
     if(request.headers.get('X-Requested-With') == 'XMLHttpRequest'):
         if request.method == 'GET':
             j_son = [
-                {'src': "/img/img1.jpg",'desc': "descripción1",'specs': ["spec11", "spec12"]},
-                {'src': "/img/img2.jpg", 'desc': "descripción2", 'specs': ["spec21", "spec22"]},
-                {'src': "/img/img3.jpg", 'desc': "descripción3", 'specs': ["spec31", "spec32"]},
+                {'clase_pais': "usa",
+				'ciudad_h2':"New York, New York",
+				'detalle_precio_total': "1,899",
+				'detalle_num_noches' : 7,
+				'precio_noche' : 275,
+				'ruta_imagen' : "static/imagenes/newyork.jpg",
+				'pie_imagen' : "Puente de Brooklyn"},
+
+                {'clase_pais': "paris",
+                 'ciudad_h2': "Paris, Francia",
+                 'detalle_precio_total': "1,499",
+                 'detalle_num_noches': 5,
+                 'precio_noche': 300,
+                 'ruta_imagen': "static/imagenes/paris.jpg",
+                 'pie_imagen': "Notre Dame de Paris"},
+
+                {'clase_pais': "uk",
+                 'ciudad_h2': "Londres, Reino Unido",
+                 'detalle_precio_total': "2,199",
+                 'detalle_num_noches': 5,
+                 'precio_noche': 440,
+                 'ruta_imagen': "static/imagenes/london.jpg",
+                 'pie_imagen': "Torre de Londres"}
+
 
             ];
             return JsonResponse({'j_son': j_son})
